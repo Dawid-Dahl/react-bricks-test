1cbe85f784f49968ddb462063c87f5747e202212
Dawid Dahl <dawiddahl@gmail.com>
2021-11-06 11:18:39 +0100

    first commit

---
+++ react-bricks/bricks/index.ts
@@ -0,0 +1,11 @@
+import { types } from 'react-bricks'
+import { website } from 'react-bricks-ui'
+import HeroUnit from './MyHeroUnit'
+
+const bricks: types.Brick<any>[] = [
+  ...website, // React Bricks UI
+  HeroUnit, // Example custom brick
+  // Put here your other bricks...
+]
+
+export default bricks
